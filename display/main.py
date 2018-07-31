13cf1e70ab8b75b47165f4075d5d4b4120e9ac91
System Administrator <root@yuqiyue-no-MacBook-Pro.local>
2018-07-31 11:09:12 +0800

    test display one image

--- display/main.py
+++ display/main.py
@@ -63,7 +63,7 @@ def main():
     frame_black = epd.get_frame_buffer(Image.open('black.bmp'))
     frame_red = epd.get_frame_buffer(Image.open('red.bmp'))
     #epd.display_frame(frame_black, None)
-    epd.display_one_image(frame_black)
+    epd.display_one_black_image(frame_black)
     #epd.display_frame(frame_black, frame_red)
 
     # You can get frame buffer from an image or import the buffer directly:
