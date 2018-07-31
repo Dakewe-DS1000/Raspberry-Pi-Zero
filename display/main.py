3318449c5ffe69fd9a8d71e8b30bde5c90e7c66f
System Administrator <root@yuqiyue-no-MacBook-Pro.local>
2018-07-31 10:16:20 +0800

    update

--- display/main.py
+++ display/main.py
@@ -60,10 +60,11 @@ def main():
     #epd.display_frame(frame_black, frame_red)
 
     # display images
-    frame_black = epd.get_frame_buffer(Image.open('black.bmp'))
+    #frame_black = epd.get_frame_buffer(Image.open('black.bmp'))
     #frame_red = epd.get_frame_buffer(Image.open('red.bmp'))
     #epd.display_frame(frame_black, None)
-    epd.display_one_image(frame_black)
+    #epd.display_one_image(frame_black)
+    epd.display_frame(None, None)
 
     # You can get frame buffer from an image or import the buffer directly:
     #epd.display_frame(imagedata.IMAGE_BLACK, imagedata.IMAGE_RED)
